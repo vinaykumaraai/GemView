0b20496b680d808570e8c23675a2aecd92056db5
Sil <sankha_sil@baxter.com>
2018-07-20 09:47:03 +0530

    making git stable

--- GemViewUI/src/main/java/com/luretechnologies/tms/backend/data/entity/AlertType.java
+++
@@ -1,9 +0,0 @@
-package com.luretechnologies.tms.backend.data.entity;
-
-public enum AlertType {
-	VOID("Void"),NOT_ACTIVE("Not Active"),REBOOT("Reboot"),REFUND("Refund"),MISC("Misc");
-	String value;
-	AlertType(String value) {
-			this.value = value;
-		}
-}
